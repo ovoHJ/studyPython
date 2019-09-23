6d208f7d829a448fb24ac7c5a3501515417a6a5b
ovoHJ <s2018w01@gmail.com>
2019-09-23 14:32:01 +0900

    내가 만든 에러 발생하고, 처리하기

---
+++ python_수행평가2_2302 강혜정/VII. 예외 처리/my_error.py
@@ -0,0 +1,16 @@
+#254
+class OddError(Exception):
+    def __init__(self, message="홀수는 ㄴㄴ"):
+        self.message = message
+
+    def __str__(self):
+        return self.message
+
+n = 11
+try:
+    if n % 2 != 0:
+        raise OddError
+    else:
+        print("짝수예요. 짝짝짝")
+except OddError as e:
+    print(e)
